fae7ccca54f314ef32eee224d073ab31db6bb641
thaliszambarda <thaliszambarda.dev@gmail.com>
2025-05-14 09:58:29 -0300

    chore: update dependencies

--- src/app/(routes)/(dashboard)/page.tsx
+++ src/app/(routes)/(dashboard)/page.tsx
@@ -1,21 +1,7 @@
-import { Suspense } from "react";
-
-import type { IHelloWorldResponse } from "@/api/mocks/get-hello-world-mock";
-import { fetchWrapper } from "@/lib/fetch";
-
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <HelloWorld />
-    </Suspense>
-  );
-}
-
-export async function HelloWorld() {
-  const data = await fetchWrapper<IHelloWorldResponse>("hello");
-  return (
     <div>
-      <h1 className="text-2xl text-red-500">{data?.welcome}</h1>
+      <h1 className="text-2xl text-red-500">Hello World</h1>
     </div>
   );
 }
