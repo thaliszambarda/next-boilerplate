f54ec56f88387f2553e82da59d54164b32f2aac5
thaliszambarda <thaliszambarda.dev@gmail.com>
2025-05-14 10:00:36 -0300

    test: remove async

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
 
-import { HelloWorld } from "@/app/(routes)/(dashboard)/page";
+import HelloWorld from "@/app/(routes)/(dashboard)/page";
 
 describe("Page", () => {
-  it("renders a heading", async () => {
-    render(await HelloWorld());
+  it("renders a heading", () => {
+    render(HelloWorld());
 
     const heading = screen.getByRole("heading", {
       level: 1,
